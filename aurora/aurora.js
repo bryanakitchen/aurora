59045fc881813b3555fbf6e1c5342a58d530a703
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-10-21 14:00:04 -0400

    add addNewNote and findById functions and hard coded data notes

--- aurora/aurora.js
+++ aurora/aurora.js
@@ -1,4 +1,4 @@
-
+import { addNewNote, pitchObject } from '../utils.js';
 //KEYS Event Listenters
 const whiteKeys = document.querySelectorAll('.white-keys');
 const blackKeys = document.querySelectorAll('.black-keys');
@@ -13,37 +13,6 @@ const keys = [...whiteKeys, ...blackKeys];
 
 const oscButton = document.getElementById('osc-key');
 
-// class Sound {
-
-//     constructor(context) {
-//         this.context = context;
-//     }
-
-//     init(type) {
-//         this.oscillator = this.context.createOscillator();
-//         this.gainNode = this.context.createGain();
-
-//         this.oscillator.connect(this.gainNode);
-//         this.gainNode.connect(this.context.destination);
-//         this.oscillator.type = type;
-//     }
-
-//     play(value, time, type) {
-//         this.init(type);
-
-//         this.oscillator.frequency.value = value;
-//         this.gainNode.gain.setValueAtTime(1, this.context.currentTime);
-
-//         this.oscillator.start(time);
-//         // this.stop(time);
-//     }
-
-//     stop(time) {
-//         this.gainNode.gain.exponentialRampToValueAtTime(0.001, time);
-//         this.oscillator.stop(time);
-//     }
-// }
-
 let oscillator;
 let gainNode;
 let release = .8;
@@ -57,7 +26,6 @@ function init(type) {
     oscillator.type = type;
     
 }
-
 
 function startSound(value, time, waveform) {
     init(waveform);
@@ -74,36 +42,9 @@ function stopSound(time) {
 
 const context = new (window.AudioContext || window.webkitAudioContext)();
 
-// oscButton.addEventListener('click', () => {
-//     let note = new Sound(context);
-//     let now = context.currentTime;
-//     note.play(261.63, now, waveform);
-// });
-
-// oscButton.addEventListener('mouseup', e => {
-//     e.target.value = note.stop();
-// }); 
-
-//Make an array of Pitches
-const pitchObject = {
-    'C3': 130.81,
-    'Cs3': 138.59,
-    'D3': 146.83,
-    'Ds3': 155.56,
-    'E3': 164.81,
-    'F3': 174.61,
-    'Fs3': 185.00,
-    'G3': 196.00,
-    'Gs3': 207.65,
-    'A3': 220.00,
-    'As3': 233.08,
-    'B3': 246.94,
-    'C4': 261.63,
-};
-
 let pitchModifier = 1;
-
-
+let resultsArray = [];
+let songArray = [];
 
 //creates a event listeners for each key
 for (let i = 0; i < keys.length; i++) {
@@ -112,6 +53,11 @@ for (let i = 0; i < keys.length; i++) {
         let now = context.currentTime;
         let currentPitch = pitchObject[keys[i].id] * pitchModifier;
         e.target.value = startSound(currentPitch, now, waveform);
+
+        let activeKey = e.target.getAttribute('id');
+
+        const test1 = addNewNote(resultsArray, activeKey);
+        // push
     });
 
     keys[i].addEventListener('mouseup', (e) => {
@@ -122,60 +68,27 @@ for (let i = 0; i < keys.length; i++) {
 
 }
 
-
 let waveform = document.querySelector(':checked').value;
 //EVENT LISTENERS FOR SYNTH WAVESHAPE PARAMETER INTERFACE
 waveformControlSine.addEventListener('click', function (event) {
 
     waveform = event.target.value;
-    // console.log(waveform);
 });
 waveformControlSquare.addEventListener('click', function (event) {
     waveform = event.target.value;
-    // console.log(waveform);
 
 });
 waveformControlTriangle.addEventListener('click', function (event) {
     waveform = event.target.value;
-    // console.log(waveform);
 
 });
 waveformControlSawtooth.addEventListener('click', function (event) {
     waveform = event.target.value;
-    // console.log(waveform);
 
 });
 
-
-
-// //waverforms
-// OscillatorNode.type = 'sine' | 'square' | 'triangle' | 'sawtooth';
-
-// //web api context
-// var context = new (window.AudioContext || window.webkitAudioContext)();
-
-// //osc node
-// var oscillator = context.createOscillator();
-
-// //waveform choice
-// oscillator.type = 'sine';
-// //osc freq or note
-// oscillator.frequency.value = 440;
-// //osc desctination
-// oscillator.connect(context.destination);
-// //osc play
-// oscillator.start();
-// //osc stop
-// oscillator.stop();
 
 // //volume knob
 // var gain = context.createGain();
 // oscillator.connect(gain);
 // gain.connect(context.destination);
-
-// //ADSR of sorts
-// var now = context.currentTime;
-// gain.gain.setValueAtTime(1, now);
-// gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5);
-// oscillator.start(now);
-// oscillator.stop(now + 0.5);
